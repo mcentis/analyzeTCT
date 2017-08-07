83c064baf1b2becb4f1b9f83c42db36bcd807fcf
Matteo Centis Vignali <vigna@pcssd47.cern.ch>
2017-08-07 14:52:41 +0200

    modified convertTxt to have the possibility to use two pulses on same ch

--- src/convertTxt.cpp
+++ src/convertTxt.cpp
@@ -1,15 +1,28 @@
 #include "iostream"
+#include "string"
 
 #include "ConversionClass.h"
 
 int main(int argc, char** argv)
 {
-  if(argc != 2){
-    std::cout << "\tUsage: convertTxt fileName" << std::endl;
+  if(argc < 2){
+    std::cout << "\tUsage: convertTxt [option] fileName" << std::endl;
+    std::cout << "\t[option]: -tp chNum -> two pulses on which channel (either 1 or 2)" << std::endl;
     return -1;
   }
+
+  std::string fileName;
+  int twoPlsCh = 0; // in case nothing specified, treat both channels
   
-  ConversionClass* ccl = new ConversionClass(argv[1]);
+  for(int i = 1; i < argc; ++i){
+    std::string arg = argv[i];
+    if(arg == "-tp")
+      twoPlsCh = atoi(argv[i++]);
+    else
+      fileName = arg;
+  }
+  
+  ConversionClass* ccl = new ConversionClass((char*) fileName.c_str(), twoPlsCh);
   ccl->ReadFile();
   ccl->DumpCondMap();
   
